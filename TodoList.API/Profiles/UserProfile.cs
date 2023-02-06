6cf2c1e4ad0dde1c37153e1f96583ce96ac623f9
david dell laptop <davidla@mta.ac.il>
2023-02-06 20:21:21 +0200

    ad user profile for auto mapper..

---
+++ TodoList.API/Profiles/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace TodoList.API.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<Models.Domain.User, Models.DTO.User>().ReverseMap();
+        }
+    }
+}
